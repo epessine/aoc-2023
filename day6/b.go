46ac91e7a65f57382116ac58e02f2fde279d8c35
Eduardo Pessine <eduardopessine@gmail.com>
2023-12-27 16:38:19 -0300

    solve 6 b

--- day6/b.go
+++ day6/b.go
@@ -1,9 +1,40 @@
 package day6
 
 import (
+	"strconv"
+	"strings"
+	"unicode"
+
 	"github.com/epessine/aoc-2023/challenge"
 )
 
 func solveB(input *challenge.Input) int {
-	return 0
+	race := Race{}
+	for line := range input.Lines() {
+		if strings.HasPrefix(line, "Time:") {
+			duration, err := strconv.Atoi(strings.Map(func(r rune) rune {
+				if unicode.IsDigit(r) {
+					return r
+				}
+				return -1
+			}, line))
+			if err != nil {
+				panic(err)
+			}
+			race.Duration = duration
+		}
+		if strings.HasPrefix(line, "Distance:") {
+			record, err := strconv.Atoi(strings.Map(func(r rune) rune {
+				if unicode.IsDigit(r) {
+					return r
+				}
+				return -1
+			}, line))
+			if err != nil {
+				panic(err)
+			}
+			race.Record = record
+		}
+	}
+	return race.WaysToWin()
 }
